b42c1a269565987ce60463fceccb8ffca22619b6
kimten88 <kimtendulkar88@gmail.com>
2022-03-29 10:54:32 -0700

    student tracking

--- studentTracking_main.py
+++ studentTracking_main.py
@@ -1,19 +1,20 @@
 # Python:   Version: 3.10.3
 # Author:   Kim Tendulkar
-# Purpose:  Phonebook Demo. Demonstrating OOP, Tkinter, GUI module
-#           using Tkinter Parent and Child relationships
+# Purpose:  STUDENT TRACKING ASSIGNMENT
+
 
 
 
 import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 
 
 
 # Be sure to import our other modules
 # So we can have access to them
-import drill50_phonebook_gui
-import drill50_phonebook_func
+import studentTracking_gui
+import studentTracking_func
 
 
 # Frame is the Tkinter frame class that our own class will inherit from
@@ -21,22 +22,22 @@ class ParentWindow(Frame):
     def __init__(self, master, *args, **kwargs):
         Frame.__init__(self, master, *args, **kwargs)
 
-        # define our master frame configurations
+    # define our master frame configurations
         self.master = master
         self.master.minsize (500, 300) #(Height, Width)
         self.master.maxsize (500, 300)
         # This CenterWindow method will center our app on the user's screen
-        phonebook_func.center_window(self, 500, 300)
-        self.master.title('The Tkinter Phonebook Demo')
+        studentTracking_func.center_window(self, 500, 300)
+        self.master.title('Student Tracking')
         self.master.configure (bg='#F0F0F0')
         # This protocol method is a tkinter built-in method to cathc if
         # the user clicks the upper corner, "X" on Windows OS
-        self.master.protocol("WM_DELETE_WINDOW", lambda: phonebook_func.ask_quit(self))
+        self.master.protocol("WM_DELETE_WINDOW", lambda: studentTracking_func.ask_quit(self))
         arg = self.master
 
         # load in the GUI widgets from a separate module,
         # keeping your code compartmentalized and clutter free
-        drill50_phonebook_gui.load_gui(self)
+        studentTracking_gui.load_gui(self)
 
 
 
@@ -45,4 +46,5 @@ if __name__ == "__main__":
     App = ParentWindow(root)
     root.mainloop()
     
+
 
